5746dc9e239c795e19fa111f333c9362f54eb785
Peter Abolude <Aboludepeter@gmail.com>
2019-07-12 21:05:30 +0100

    first PR

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require("Clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "bfd0393d7b014c2a921a511397305c8d"
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json("Unable to work with Api"));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("Unable to get count"));
+};
+
+module.exports = {
+  handleImage,
+  handleApiCall
+};
